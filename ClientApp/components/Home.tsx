e966cd5abe34886163b9d547d7e6c465294d47f4
Bruce LANE <bruce@batchass.fr>
2018-07-02 21:55:36 +0200

    biot

--- ClientApp/components/Home.tsx
+++ ClientApp/components/Home.tsx
@@ -6,7 +6,7 @@ export class Home extends React.Component<RouteComponentProps<{}>, {}> {
         return <div>
             <h1>Sophia Digital Art</h1>
             <p><a href="/antibes.html">Antibes</a></p>
-            <p><a href="/antibes2.html">Antibes2</a></p>
+            <p><a href="/biot.html">Biot</a></p>
             <p><a href="/vuebase.html">Vue de base</a></p>
             <p><a href="/version2.html">Saint-Philippe Version 2</a></p>
             
